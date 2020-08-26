bb99633735adb10a05668a4f2625d6118febe77e
Tùng Nguyễn <tung.nguyen21098@gmail.com>
2020-08-26 10:48:26 +0700

    Updates

---
+++ precache-manifest.91b9fb02a0d5be69e24bb33df9ce0693.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5372270fc228c86c532c5613b7c49d47",
+    "url": "/clone-instagram/index.html"
+  },
+  {
+    "revision": "ebb5d1923476dce8c434",
+    "url": "/clone-instagram/static/css/main.a8e7449b.chunk.css"
+  },
+  {
+    "revision": "69abba048c780fee8a04",
+    "url": "/clone-instagram/static/js/2.ddf32b53.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/clone-instagram/static/js/2.ddf32b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb5d1923476dce8c434",
+    "url": "/clone-instagram/static/js/main.40ab867a.chunk.js"
+  },
+  {
+    "revision": "e1c5e15e68cd429e6151",
+    "url": "/clone-instagram/static/js/runtime-main.2eac7397.js"
+  },
+  {
+    "revision": "f06b908907d5d4f2aaf733e2bee7ea8e",
+    "url": "/clone-instagram/static/media/chplay.f06b9089.png"
+  },
+  {
+    "revision": "692d5ec6a2bac48dea82edd829b88660",
+    "url": "/clone-instagram/static/media/close.692d5ec6.svg"
+  },
+  {
+    "revision": "a971e307ad4eb00d671986201d184be0",
+    "url": "/clone-instagram/static/media/comment.a971e307.svg"
+  },
+  {
+    "revision": "c2aa4c723af21a03520967ca132b1a93",
+    "url": "/clone-instagram/static/media/facebook-blue.c2aa4c72.svg"
+  },
+  {
+    "revision": "700dba7024b41484270b6d1dc85d2f7d",
+    "url": "/clone-instagram/static/media/facebook.700dba70.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/clone-instagram/static/media/heart.a8cd76c1.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/clone-instagram/static/media/heart_black.70ebbcd1.svg"
+  },
+  {
+    "revision": "aafd8c6b005d7d971fad0012daa374d3",
+    "url": "/clone-instagram/static/media/img_slide_index.aafd8c6b.jpg"
+  },
+  {
+    "revision": "38825c9d5aa205d7a08cd2f2674b4f69",
+    "url": "/clone-instagram/static/media/img_slide_index1.38825c9d.png"
+  },
+  {
+    "revision": "2d9d7248af43c6a4405960bfb0254d48",
+    "url": "/clone-instagram/static/media/img_slide_index2.2d9d7248.jpg"
+  },
+  {
+    "revision": "629d23a3c7b24459b2584bddb8a4a8e5",
+    "url": "/clone-instagram/static/media/img_slide_index4.629d23a3.jpg"
+  },
+  {
+    "revision": "001bc33056c10fdbbdb1db41009b57e1",
+    "url": "/clone-instagram/static/media/img_slide_index5.001bc330.jpg"
+  },
+  {
+    "revision": "f5ae123ab1e24e72615bea84fc7b4845",
+    "url": "/clone-instagram/static/media/img_slide_index6.f5ae123a.jpg"
+  },
+  {
+    "revision": "bc33aae8be4f6d8ed7ac318ae538cd2c",
+    "url": "/clone-instagram/static/media/instagram-new-logo.bc33aae8.png"
+  },
+  {
+    "revision": "8d7d7cee39c4a26c71a83bfdfdbadeb7",
+    "url": "/clone-instagram/static/media/loading.8d7d7cee.gif"
+  },
+  {
+    "revision": "f51e4fb9dc00c00ec97df33a716a4e30",
+    "url": "/clone-instagram/static/media/loadingCat.f51e4fb9.gif"
+  },
+  {
+    "revision": "7457cb2b0341b07871217ee4fdd3644f",
+    "url": "/clone-instagram/static/media/share.7457cb2b.svg"
+  }
+]);
